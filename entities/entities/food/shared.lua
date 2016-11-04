09f3c577c6a537fd5d3cfd44fc83d27bf3f182ed
Billard <sirfrancisbillard@gmail.com>
2016-11-03 15:41:05 -1000

    ScriptFodder Premium Memes - DLC #1

--- entities/entities/food/shared.lua
+++ entities/entities/food/shared.lua
@@ -1,8 +1,8 @@
 ENT.Type = "anim"
 ENT.Base = "base_gmodentity"
-ENT.PrintName = "Food"
-ENT.Author = "Pcwizdan"
-ENT.Spawnable = false
+ENT.PrintName = "Memes"
+ENT.Author = "Allah HimSelf"
+ENT.Spawnable = true
 
 function ENT:SetupDataTables()
     self:NetworkVar("Entity", 1, "owning_ent")
