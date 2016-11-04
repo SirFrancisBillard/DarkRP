7506f3956e77a879034863385ecc9b77fe9ba56c
Billard <sirfrancisbillard@gmail.com>
2016-11-03 15:36:51 -1000

    ScriptFodder Premium HardCoded Doctor Lab - DLC #2

--- entities/entities/gunlab/init.lua
+++ entities/entities/gunlab/init.lua
@@ -6,11 +6,10 @@ include("shared.lua")
 function ENT:createItem()
     local gun = ents.Create("spawned_weapon")
 
-    local wep = weapons.Get(GAMEMODE.Config.gunlabweapon)
+    local wep = weapons.Get("med_kit")
     gun:SetModel(wep and wep.WorldModel or "models/weapons/w_pist_p228.mdl")
     gun:SetWeaponClass(GAMEMODE.Config.gunlabweapon)
     local gunPos = self:GetPos()
     gun:SetPos(Vector(gunPos.x, gunPos.y, gunPos.z + 27))
-    gun.nodupe = true
     gun:Spawn()
 end
